c10437eb0ba09513cb49f6c92d5af48685301be5
taohua656 <yan.zhenqi@163.com>
2026-04-20 18:27:00 +0800

    3.0.6

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,12 @@
 buildscript {
+	val kotlinVersion by extra("1.8.22)
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.0")
+		classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
